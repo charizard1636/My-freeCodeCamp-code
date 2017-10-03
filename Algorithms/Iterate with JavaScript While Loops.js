5cc0c17047a4a649dd57f3a48f656ce731f8a206
Linda Lam <llam7300@gmail.com>
2017-10-02 21:16:51 -0400

    Create Iterate with JavaScript While Loops.js

---
+++ Algorithms/Iterate with JavaScript While Loops.js
@@ -0,0 +1,27 @@
+/* Iterate with JavaScript While Loops 
+
+You can run the same code multiple times by using a loop.
+
+Another type of JavaScript loop is called a "while loop", because it runs "while" a specified condition is true and stops once that condition is no longer true.
+
+var ourArray = [];
+var i = 0;
+while(i < 5) {
+  ourArray.push(i);
+  i++;
+}
+Let's try getting a while loop to work by pushing values to an array.
+
+Instructions
+Push the numbers 0 through 4 to myArray using a while loop.
+
+Run tests (ctrl + enter)
+*/
+
+var myArray = [];
+//Push the numbers zero through four to myArray
+var i = 0;
+while(i<5) {
+  myArray.push(i);
+  i++;
+}
