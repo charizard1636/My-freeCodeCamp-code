bd63f9782c0359ab888543251ea5dcc3621c0f90
Linda Lam <llam7300@gmail.com>
2017-10-03 21:25:15 -0400

    Create Declare JavaScript Objects as Variables.js

---
+++ Algorithms/Declare JavaScript Objects as Variables.js
@@ -0,0 +1,27 @@
+/* Declare JavaScript Objects as Variables 
+
+Before we dive into Object Oriented Programming, let's revisit JavaScript objects.
+
+Give your motorBike object a wheels, engines and seats attribute and set them to numbers.
+*/
+
+//Here is a sample Object
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+//Now Let's make a similar Object called motorBike
+//Give it two wheels, one engine and one seat
+var motorBike = {
+  // Only change code below this line.
+  "wheels":2,
+  "engines":1,
+  "seats":1
+
+
+  // Only change code above this line.
+};
+
+(function() {return JSON.stringify(motorBike);})();
