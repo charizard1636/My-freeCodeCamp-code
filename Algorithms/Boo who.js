fff64959a9b2895bc051d4210626e73c852ad5fb
Linda Lam <llam7300@gmail.com>
2017-11-06 21:06:05 -0500

    Create Boo who.js

---
+++ Algorithms/Boo who.js
@@ -0,0 +1,17 @@
+/* Boo who
+
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+
+Here are some helpful links:
+
+Boolean Objects
+*/
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return typeof bool === 'boolean';
+}
+
+booWho(null);
