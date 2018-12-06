8dd922540ddc887be8ca7f6c0467074994fadc19
Linda Lam <llam7300@gmail.com>
2018-12-06 09:19:50 -0500

    Create Basic Data Structures: Access an Array's Contents Using Bracket Notation.js

---
+++ Basic Data Structures/Basic Data Structures: Access an Array's Contents Using Bracket Notation.js
@@ -0,0 +1,7 @@
+/* In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides "b". */
+
+let myArray = ["a", "b", "c", "d"];
+// change code below this line
+myArray[1] = "e";
+//change code above this line
+console.log(myArray);
