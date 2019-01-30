c0d87493124f3e2e1ac0841081bebfacf3078568
Linda Lam <llam7300@gmail.com>
2019-01-30 12:35:15 -0500

    Create Intermediate Algorithm Scripting: Spinal Tap Case.js

---
+++ Intermediate Algorithm Scripting/Intermediate Algorithm Scripting: Spinal Tap Case.js
@@ -0,0 +1,10 @@
+/* Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes. */
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  var newStr = [];
+  return newStr = str.split(/\s|_|(?=[A-Z])/).join("-").toLowerCase();
+}
+
+spinalCase('This Is Spinal Tap');
