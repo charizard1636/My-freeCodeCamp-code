cf485f7358e9bc611865c024441a5fe8b3b6b601
Linda Lam <llam7300@gmail.com>
2019-02-13 21:48:36 -0500

    Create Intermediate Algorithm Scripting: Smallest Common Multiple.js

---
+++ Intermediate Algorithm Scripting/Intermediate Algorithm Scripting: Smallest Common Multiple.js
@@ -0,0 +1,27 @@
+/* Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+The range will be an array of two numbers that will not necessarily be in numerical order.
+
+For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6. */
+
+function smallestCommons(arr) {
+var range = [];
+for (var i = Math.max(arr[0], arr[1]); i >= Math.min(arr[0], arr[1]); i--) {
+range.push(i);
+}
+
+var lcm = range[0];
+for (i = 1; i < range.length; i++) {
+var GCD = gcd(lcm, range[i]);
+lcm = (lcm * range[i]) / GCD;
+}
+return lcm;
+
+function gcd(a, b) { 
+if (b === 0)
+return a;
+else
+return gcd(b, a%b);
+}
+}
+smallestCommons([1,5]);
